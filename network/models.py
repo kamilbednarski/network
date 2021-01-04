01f68ebdc3f043f975bf91c59f493c6b6a1b1e90
Kamil Bednarski <kamilarturbednarski@gmail.com>
2021-01-04 21:49:36 +0100

    update:models

--- network/models.py
+++ network/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     pass
 
 class Post(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=140)
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -19,18 +19,18 @@ class Post(models.Model):
         }
 
     def __str__(self):
-        return "post with id: " + self.id + " from user: " + self.user_id
+        return "post with id: " + str(self.id) + " from user: " + str(self.user)
 
 class Like(models.Model):
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "user with id: " + self.user_id + " likes post with id: " + self.post_id
+        return "user with id: " + str(self.user_id) + " likes post with id: " + str(self.post_id)
 
 class Relation(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     friend_id = models.ManyToManyField(User, related_name='friend')
 
     def __str__(self):
-        return "user with id: " + self.user_id + " follows user with id: " + self.friend_id
+        return "user with id: " + str(self.user_id) + " follows user with id: " + str(self.friend_id)
