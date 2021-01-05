ec1463d71ae84b3d3955b4e294ecc41db85b80ae
Kamil Bednarski <kamilarturbednarski@gmail.com>
2021-01-05 19:14:01 +0100

    update:user model - following, followedby counters

--- network/models.py
+++ network/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followed_by_count = models.IntegerField(default=0)
+    following_count = models.IntegerField(default=0)
+
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -21,12 +23,14 @@ class Post(models.Model):
     def __str__(self):
         return "post with id: " + str(self.id) + " from user: " + str(self.user)
 
+
 class Like(models.Model):
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return "user with id: " + str(self.user_id) + " likes post with id: " + str(self.post_id)
+
 
 class Relation(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
