71eb6bb2582593f3709e31392e0e1da909c28e67
Kamil Bednarski <kamilarturbednarski@gmail.com>
2021-01-05 19:16:09 +0100

    update: user model posts count added to optimize db queries

--- network/models.py
+++ network/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(AbstractUser):
     followed_by_count = models.IntegerField(default=0)
     following_count = models.IntegerField(default=0)
+    posts_count = models.IntegerField(default=0)
 
 
 class Post(models.Model):
