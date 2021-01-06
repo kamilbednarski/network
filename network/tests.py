1b092252840a495189733ab21fa52dd0818f92ee
Kamil Bednarski <kamilarturbednarski@gmail.com>
2021-01-06 18:14:55 +0100

    new: tests for Post object

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,4 @@
+import re
 from django.test import TestCase
 from network.models import User, Post, Like, Relation
 
@@ -7,7 +8,7 @@ class UserTestCase(TestCase):
     Tests for User model.
     '''
     def set_up_new_test_user(self):
-        first_test_user = User.objects.create(
+        test_user = User.objects.create(
             username='testuser',
             email='testuser@testcase.com',
             password='testpassword'
@@ -37,9 +38,9 @@ class UserTestCase(TestCase):
         second_test_user = User.objects.get(username='secondtestuser')
 
 
-    def test_users_fields_have_correct_values(self):
+    def test_users_variables_have_correct_values(self):
         '''
-        Checks if created users' fields have correct values.
+        Checks if created users' variables have correct values.
         '''
         self.set_up_new_test_users()
         first_test_user = User.objects.get(username='firsttestuser')
@@ -110,3 +111,60 @@ class UserTestCase(TestCase):
 
         test_user.decrement_number_of_followers()
         self.assertEqual(test_user.get_number_of_followers(), 0)
+
+
+class PostTestCase(TestCase):
+    '''
+    Tests for Post model.
+    '''
+    def set_up_new_test_user(self):
+        test_user = User.objects.create(
+            username='testuser',
+            email='testuser@testcase.com',
+            password='testpassword'
+        )
+
+
+    def set_up_new_test_post(self):
+        self.set_up_new_test_user()
+        test_user = User.objects.get(username='testuser')
+
+        test_post = Post.objects.create(
+            user=test_user,
+            content='This is test content of test post.'
+        )
+
+
+    def test_post_was_correctly_created(self):
+        '''
+        Checks if post was created correctly.
+        '''
+        self.set_up_new_test_post()
+        test_post = Post.objects.get(id=1)
+
+
+    def test_posts_content_variable_has_correct_value(self):
+        '''
+        Checks if created post's content variable has correct value.
+        '''
+        self.set_up_new_test_post()
+        test_post = Post.objects.get(id=1)
+
+        self.assertEqual(test_post.content, 'This is test content of test post.')
+
+    def test_posts_timestamp_is_valid(self):
+        '''
+        Checks if variable timestamp is valid timestamp.
+        '''
+        self.set_up_new_test_post()
+        test_post = Post.objects.get(id=1)
+        timestamp = str(test_post.timestamp)
+
+        status = False
+        if re.match('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', timestamp):
+            status = True
+
+        self.assertEqual(status, True)
+
+    def test_posts_serialization(self):
+        pass
