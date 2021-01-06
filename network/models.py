7c2705eb020546991da72b7b1519b1e439355156
Kamil Bednarski <kamilarturbednarski@gmail.com>
2021-01-06 15:21:36 +0100

    update: models representation

--- network/models.py
+++ network/models.py
@@ -3,9 +3,18 @@ from django.db import models
 
 
 class User(AbstractUser):
-    followed_by_count = models.IntegerField(default=0)
-    following_count = models.IntegerField(default=0)
-    posts_count = models.IntegerField(default=0)
+    followed_by_count = models.PositiveIntegerField(default=0)
+    following_count = models.PositiveIntegerField(default=0)
+    posts_count = models.PositiveIntegerField(default=0)
+
+    def get_number_of_posts(self):
+        return self.posts_count
+
+    def get_number_of_followers(self):
+        return self.followed_by_count
+
+    def get_number_of_followed_by_user(self):
+        return self.following_count
 
 
 class Post(models.Model):
@@ -22,20 +31,33 @@ class Post(models.Model):
         }
 
     def __str__(self):
-        return "post with id: " + str(self.id) + " from user: " + str(self.user)
+        return ("post with id: "
+                + str(self.id)
+                + " from user: "
+                + f"'{str(self.user)}'")
 
 
 class Like(models.Model):
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "user with id: " + str(self.user_id) + " likes post with id: " + str(self.post_id)
+        return ("user: "
+                + f"'{str(self.user)}'"
+                + " likes "
+                + str(self.post))
 
 
 class Relation(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-    friend_id = models.ManyToManyField(User, related_name='friend')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    users_friend = models.ManyToManyField(User, related_name='friend')
+
+    def get_users_friend(self):
+        users_friend = User.objects.get(id=self.id)
+        return users_friend
 
     def __str__(self):
-        return "user with id: " + str(self.user_id) + " follows user with id: " + str(self.friend_id)
+        return ("user: "
+                + f"'{str(self.user)}'"
+                + " follows user: "
+                + f"'{str(self.get_users_friend())}'")
