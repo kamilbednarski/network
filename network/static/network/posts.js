2bef5588e420c09e3acdd3b30f5884dcac368c8e
Kamil Bednarski <kamilarturbednarski@gmail.com>
2021-01-13 16:32:30 +0100

    update: posts.js loadSinglePost and loadPosts

--- network/static/network/posts.js
+++ network/static/network/posts.js
@@ -1,55 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#load-all-posts').addEventListener('click' , () => {
         document.querySelector('#posts-view').innerHTML = ''
-        load_posts(1)
+        document.querySelector('#body-view').style.display = 'none'
+        document.querySelector('#posts-view').style.display = 'block'
+        loadPosts(1)
         document.querySelector('#navbar-toggler-button').click()
     })
 });
 
-function load_single_post(postId) {
-    fetch(`/post/id/${postId}`, {method: 'GET'})
-    .then(response => response.json())
-    .then(post => {
-        console.log(post['id'])
-        if(post['id'] != undefined) {
-            let mainPostsContainer = document.querySelector('#posts-view')
+async function loadSinglePost(postId) {
+    const response = await fetch(`/post/id/${postId}`, { method: 'GET' })
+    const post = await response.json()
+    console.log(post['id'])
+    if (post['id'] != undefined) {
+        let mainPostsContainer = document.querySelector('#posts-view')
 
-            let postContainer = document.createElement('div')
-            postContainer.classList.add('card', 'm-3')
+        let postContainer = document.createElement('div')
+        postContainer.classList.add('card', 'm-3')
 
-            let userAndDateContainer = document.createElement('div')
-            userAndDateContainer.classList.add('card-header')
+        let userAndDateContainer = document.createElement('div')
+        userAndDateContainer.classList.add('card-header')
 
-            let userContainer = document.createElement('span')
-            userContainer.classList.add('fw-bold')
+        let userContainer = document.createElement('span')
+        userContainer.classList.add('fw-bold')
 
-            let dateContainer = document.createElement('span')
-            dateContainer.classList.add('small', 'fw-light', 'text-uppercase')
+        let dateContainer = document.createElement('span')
+        dateContainer.classList.add('small', 'fw-light', 'text-uppercase')
 
-            let contentContainer = document.createElement('div')
-            contentContainer.classList.add('p-3')
+        let contentContainer = document.createElement('div')
+        contentContainer.classList.add('p-3')
 
-            userContainer.innerText = '@' + post['user'] + ' · '
-            dateContainer.innerText = post['date_added']
-            contentContainer.innerText = post['content']
+        userContainer.innerText = '@' + post['user'] + ' · '
+        dateContainer.innerText = post['date_added']
+        contentContainer.innerText = post['content']
 
-            userAndDateContainer.append(userContainer, dateContainer)
-            postContainer.append(userAndDateContainer, contentContainer)
-            mainPostsContainer.append(postContainer)
-        }
-    })
+        userAndDateContainer.append(userContainer, dateContainer)
+        postContainer.append(userAndDateContainer, contentContainer)
+        mainPostsContainer.append(postContainer)
+    }
 }
 
-function load_posts(startNumber) {
+function loadPosts(postId) {
+    let startNumber = postId
     let endNumber = startNumber + 9
-    while(startNumber <= endNumber) {
-        load_single_post(startNumber)
-        startNumber++
+
+    const loadMultiplePosts = async _ => {
+        for(let i = startNumber; i <= endNumber; i++) {
+            const loadedPost = await loadSinglePost(startNumber)
+            startNumber++
+        }
     }
+
+    loadMultiplePosts()
 
     window.addEventListener('scroll', () => {
         if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
-            load_posts(startNumber)
+            loadPosts(startNumber)
         }
     })
 }
---
+++ network/static/network/posts_old.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#load-all-posts').addEventListener('click' , () => {
+        document.querySelector('#posts-view').innerHTML = ''
+        document.querySelector('#body-view').style.display = 'none'
+        document.querySelector('#posts-view').style.display = 'block'
+        load_posts(1)
+        document.querySelector('#navbar-toggler-button').click()
+    })
+});
+
+function load_single_post(postId) {
+    fetch(`/post/id/${postId}`, {method: 'GET'})
+    .then(response => response.json())
+    .then(post => {
+        console.log(post['id'])
+        if(post['id'] != undefined) {
+            let mainPostsContainer = document.querySelector('#posts-view')
+
+            let postContainer = document.createElement('div')
+            postContainer.classList.add('card', 'm-3')
+
+            let userAndDateContainer = document.createElement('div')
+            userAndDateContainer.classList.add('card-header')
+
+            let userContainer = document.createElement('span')
+            userContainer.classList.add('fw-bold')
+
+            let dateContainer = document.createElement('span')
+            dateContainer.classList.add('small', 'fw-light', 'text-uppercase')
+
+            let contentContainer = document.createElement('div')
+            contentContainer.classList.add('p-3')
+
+            userContainer.innerText = '@' + post['user'] + ' · '
+            dateContainer.innerText = post['date_added']
+            contentContainer.innerText = post['content']
+
+            userAndDateContainer.append(userContainer, dateContainer)
+            postContainer.append(userAndDateContainer, contentContainer)
+            mainPostsContainer.append(postContainer)
+        }
+    })
+}
+
+function load_posts(startNumber) {
+    let endNumber = startNumber + 9
+    while(startNumber <= endNumber) {
+        load_single_post(startNumber)
+        startNumber++
+    }
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+            load_posts(startNumber)
+        }
+    })
+}
